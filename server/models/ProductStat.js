c421096607fe616ce1a8a23925c3a856f3440188
KarlKo1 <kongopoiss@gmail.com>
2023-02-07 19:54:56 +0200

    Add products and productsstat model

---
+++ server/models/ProductStat.js
@@ -0,0 +1,26 @@
+import mongoose from "mongoose";
+
+const ProductStatSchema = new mongoose.Schema(
+  {
+    // In real apps make them required
+    productId: String,
+    yearlySalesTotal: Number,
+    yearlyTotalSoldUnits: Number,
+    year: Number,
+    monthlyData: {
+      month: String,
+      totalSales: Number,
+      totalUnits: Number,
+    },
+
+    dailyData: {
+      date: String,
+      totalSales: Number,
+      totalUnits: Number,
+    },
+  },
+  { timestamps: true }
+);
+
+const ProductStat = mongoose.model("ProductStat", ProductStatSchema);
+export default ProductStat;
